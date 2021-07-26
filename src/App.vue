b74e177f81f26e9648cb2afb12cee9be7399cd8e
Pedro Gustavo <eu@pedrogustavo.com>
2021-07-26 06:42:12 -0300

    :fire: Remove links de navegação

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
     <router-view/>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/state">State</router-link>
-    </div>
   </div>
 </template>
 
