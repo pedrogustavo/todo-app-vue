41aa1252a8c59903dc83157dff87c10aab58f559
Pedro Gustavo <eu@pedrogustavo.com>
2021-07-26 06:44:36 -0300

    :sparkles: Adiciona componente de exemplo de databinding

---
+++ src/components/ComponentClass.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Batatinha quando nasce</h1><br>
+        <input type="text" v-model="value" />
+        <br>
+        <br>
+        <p>{{value}}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Batatinha',
+    data () {
+        return {
+            value: 'Hello World'
+        }
+    },
+    created () {
+        // ...
+    },
+    beforeDestroy () {
+        // ...
+    }
+}
+</script>
---
+++ src/views/Binding.vue
@@ -0,0 +1,13 @@
+<template>
+    <component-class />
+</template>
+<script>
+import ComponentClass from '../components/ComponentClass.vue'
+
+export default {
+    name: 'Binding',
+    components: {
+        ComponentClass
+    }
+}
+</script>
